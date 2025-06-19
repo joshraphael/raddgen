52ece09168d23d4679a59710ca06a40fb23329d7
Joshua Raphael <josh.n.raphael@gmail.com>
2025-06-18 22:30:47 -0700

    add initial call and validation to RA GetGameExtended endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/joshraphael/raddgen
 
 go 1.23.2
+
+require (
+	github.com/joshraphael/go-retroachievements v1.0.4
+	github.com/nao1215/markdown v0.8.0
+)
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/karrick/godirwalk v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/errors v0.0.0-20250405072817-4e6d85265da6 // indirect
+	github.com/olekukonko/ll v0.0.8 // indirect
+	github.com/olekukonko/tablewriter v1.0.7 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joshraphael/go-retroachievements"
+	"github.com/joshraphael/go-retroachievements/models"
+	"github.com/nao1215/markdown"
+)
 
 func main() {
-	fmt.Println("hello world")
+	if len(os.Args) <= 1 {
+		log.Fatalf("need at least one argument")
+	}
+	id := os.Args[1]
+	gameID, err := strconv.Atoi(id)
+	if err != nil {
+		log.Fatalf("Argument must be an integer: %s", id)
+	}
+	secret := os.Getenv("RA_API_KEY")
+
+	client := retroachievements.NewClient(secret)
+
+	resp, err := client.GetGameExtended(models.GetGameExtentedParameters{
+		GameID: gameID,
+	})
+	if err != nil {
+		log.Fatalf("Error on RA call GetGameExtended with game id %d: %s", gameID, err.Error())
+	}
+
+	if resp == nil {
+		log.Fatalf("No game found for id %d", gameID)
+	}
+
+	markdown.NewMarkdown(os.Stdout).
+		H1(resp.Title).Build()
 }
