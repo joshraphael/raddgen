70fd45bbffa75e4949b2d562694f48d088c0ef5d
Joshua Raphael <josh.n.raphael@gmail.com>
2025-06-19 00:33:42 -0700

    add achievmeent sorting and image downloads

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 run:
+	mkdir -p out
 	go run main.go 4111 > out/DESIGN.md
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"sort"
 	"strconv"
 
 	"github.com/joshraphael/go-retroachievements"
@@ -36,10 +39,21 @@ func main() {
 	}
 
 	achievementTableOfContents := []string{}
+	sortedAchievements := []models.GetGameExtentedAchievement{}
 
 	for i := range resp.Achievements {
 		achievement := resp.Achievements[i]
+		sortedAchievements = append(sortedAchievements, achievement)
+	}
+
+	sort.Slice(sortedAchievements, func(i, j int) bool {
+		return sortedAchievements[i].DisplayOrder < sortedAchievements[j].DisplayOrder
+	})
+
+	for i := range sortedAchievements {
+		achievement := sortedAchievements[i]
 		achievementTableOfContents = append(achievementTableOfContents, markdown.Link(fmt.Sprintf("%s (Achievement %d)", achievement.Title, achievement.ID), fmt.Sprintf("#achievement-%d", achievement.ID)))
+		downloadImage(achievement.BadgeName, "out/badges")
 	}
 
 	doc := markdown.NewMarkdown(os.Stdout)
@@ -58,8 +72,8 @@ func main() {
 	doc.H2("Achievements")
 	doc.PlainTextf("<sub>%s</sub>", markdown.Link("Back to Table of Contents", "#table-of-contents"))
 	doc.OrderedList(achievementTableOfContents...)
-	for i := range resp.Achievements {
-		achievement := resp.Achievements[i]
+	for i := range sortedAchievements {
+		achievement := sortedAchievements[i]
 		addAchievement(doc, achievement)
 	}
 	// Do stuff here
@@ -70,4 +84,46 @@ func addAchievement(doc *markdown.Markdown, achievement models.GetGameExtentedAc
 	doc.H3f(markdown.Link(fmt.Sprintf("Achievement %d", achievement.ID), fmt.Sprintf("https://retroachievements.org/achievement/%d", achievement.ID)))
 	doc.PlainTextf("Title: %s", markdown.Bold(achievement.Title))
 	doc.PlainText(fmt.Sprintf("<br>Points: %s", markdown.Bold(fmt.Sprintf("%d", achievement.Points))))
+	doc.PlainText(fmt.Sprintf("<br>%s", markdown.Image(achievement.Title, fmt.Sprintf("badges/%s.png", achievement.BadgeName))))
+}
+
+func downloadImage(imageID, downloadPath string) (image string, err error) {
+	url := fmt.Sprintf("https://media.retroachievements.org/Badge/%s.png", imageID)
+	head, e := http.Head(url)
+	if e != nil {
+		return "", err
+	}
+	defer head.Body.Close()
+	if head.StatusCode == http.StatusForbidden {
+		return "", nil
+	}
+	if head.StatusCode == http.StatusOK {
+		response, e := http.Get(url)
+		if e != nil {
+			return "", err
+		}
+		defer response.Body.Close()
+		if response.StatusCode == http.StatusOK {
+			err := os.MkdirAll(downloadPath, 0777)
+			if err != nil {
+				return "", err
+			}
+			//open a file for writing
+			file_name := fmt.Sprintf("%s/%s.png", downloadPath, imageID)
+			file, err := os.Create(file_name)
+			if err != nil {
+				return "", err
+			}
+			defer file.Close()
+
+			// Use io.Copy to just dump the response body to the file. This supports huge files
+			_, err = io.Copy(file, response.Body)
+			if err != nil {
+				return "", err
+			}
+			return file_name, nil
+		}
+		return "", fmt.Errorf("unknown response from GET %s, got: %d", url, response.StatusCode)
+	}
+	return "", fmt.Errorf("unknown response from HEAD %s, got: %d", url, head.StatusCode)
 }
